df6638d5d226f58d7821e013555854ba71f2d97f
nirbhay165 <162484986+nirbhay165@users.noreply.github.com>
2024-06-05 23:09:29 +0530

    Delete project directory

--- project/js2.js
+++
@@ -1,7 +0,0 @@
-function fun1(){
-    var name = frm1.name.value;
-    var gender = frm1.gender.value;
-    var cource = frm1.cource.value;
-    var remark = frm1.remark.value;
-    alert("Name: "+name+"\nGender: "+gender+"\nCource: "+cource+"\nRemark: "+remark);
-}
